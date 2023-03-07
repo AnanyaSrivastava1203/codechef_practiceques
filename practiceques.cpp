05e22c5aafc8f7ce6456469b4e27bc81d2b94f91
AnanyaSrivastava1203 <sriananya1203@gmail.com>
2023-03-07 22:03:36 +0530

    patteren

---
+++ practiceques.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi 3.14
+#define mod 1000000007
+#define nl '\n'
+#define int long long int
+const int N = 1e5 + 10;
+
+signed main(){
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
